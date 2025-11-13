17cdbfef963a03fe60f234f18588c1aef76bb673
AKMZakaria <akmzakaria488@gmail.com>
2025-11-13 15:41:50 +0600

    aos update

--- src/Components/PopularCourses.jsx
+++ src/Components/PopularCourses.jsx
@@ -10,12 +10,14 @@ const PopularCourses = ({ data }) => {
     .sort((a, b) => b.price - a.price)
 
   return (
-    <div data-aos="fade-up">
+    <div>
       <h3 className="text-xl md:text-4xl mt-10 font-bold text-center">Popular Courses</h3>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center md:max-w-[1400px] mx-auto">
         {featuredCourses.map((course) => (
           <div
+            data-aos="fade-up"
+            data-aos-anchor-placement="center-bottom"
             key={course.price}
             className="flex flex-col shadow rounded-xl w-[300px] md:w-[300px] gap-2 p-4 bg-base-200  mt-5"
           >
